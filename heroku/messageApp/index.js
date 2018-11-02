dab7388483078782d4d94d528e00a268160a80ae
Arnar Leifsson <drleifz@gmail.com>
2018-11-02 00:09:23 +0000

    Removing messageApp

--- heroku/messageApp/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/api/message', (req, res) => {
-    return res.json(process.env.ENVIRONMENTAL_MESSAGE);
-});
-
-app.listen(process.env.PORT, () => console.log('Listening!'));
